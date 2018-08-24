efd571c3ea705698ad56dd56192b12b37df4474e
Rohit Kumar <rohitkpa@gmail.com>
2018-08-24 10:50:42 -0700

    creation of data package, which includes all stock data timings

---
+++ quantfi-backend/src/main/java/data/StockData.java
@@ -0,0 +1,9 @@
+package connection;
+
+public class StockData {
+    private final ApiConnector connector;
+
+    public StockData(ApiConnector connector) {
+        this.connector = connector;
+    }
+}
