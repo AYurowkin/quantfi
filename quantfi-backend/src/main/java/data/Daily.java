5dd6243b42ee01ae17e969d4b33f4cb7761b033e
Rohit Kumar <rohitkpa@gmail.com>
2018-08-27 13:33:29 -0700

    Added foundation for stock information. Moved StockData as a feature.

--- quantfi-backend/src/main/java/data/Daily.java
+++ quantfi-backend/src/main/java/data/Daily.java
@@ -1,4 +1,17 @@
 package data;
 
-public class Daily {
+import java.util.List;
+import java.util.Map;
+
+public class Daily extends StockDataResponse {
+
+    private Daily(final Map<String, String> metaData,final List<StockInfo> stockInfoList) {
+        super(metaData, stockInfoList);
+    }
+
+//    public static Daily from(String json) {
+//        Parser parser = new Parser();
+//        return parser.parseJson(json);
+//    }
+
 }
