0332c7afca575bf64e36ad0c68dc8651e8ad1e30
Rohit Kumar <rohitkpa@gmail.com>
2018-08-30 16:50:43 -0700

    Updated DailyAdjusted, IntraDay. Created Interval

--- quantfi-backend/src/main/java/data/DailyAdjusted.java
+++ quantfi-backend/src/main/java/data/DailyAdjusted.java
@@ -1,4 +1,50 @@
 package data;
 
-public class DailyAdjusted {
+import connection.AlphaVantageException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DailyAdjusted extends StockDataResponse {
+
+    private DailyAdjusted(final Map<String, String> metaData, final List<StockInfo> stockInfoList) {
+        super(metaData, stockInfoList);
+    }
+
+    public static DailyAdjusted from(String json) {
+        Parser parser = new Parser();
+        return parser.parseJson(json);
+    }
+
+    private static class Parser extends StockDataParser<DailyAdjusted> {
+
+        @Override
+        String getStockDataKey() {
+            return "Stock Data (DailyAdjusted)";
+        }
+
+        @Override
+        DailyAdjusted resolve(Map<String, String> metaData, Map<String, Map<String, String>> stockData) {
+            List<StockInfo> stocks = new ArrayList<>();
+
+            try {
+                stockData.forEach((key, values) -> stocks.add(new StockInfo(
+                        LocalDate.parse(key, DATE_SIMPLE_FORMAT).atStartOfDay(),
+                        Double.parseDouble(values.get("1. open")),
+                        Double.parseDouble(values.get("2. high")),
+                        Double.parseDouble(values.get("3. low")),
+                        Double.parseDouble(values.get("4. close")),
+                        Double.parseDouble(values.get("5. adjusted close")),
+                        Long.parseLong(values.get("6. volume")),
+                        Double.parseDouble(values.get("7. dividend amount")),
+                        Double.parseDouble(values.get("8. split coefficient"))
+                )));
+            } catch (Exception e) {
+                throw new AlphaVantageException("API DailyAdjusted StockInfo error");
+            }
+            return new DailyAdjusted(metaData, stocks);
+        }
+    }
 }
--- quantfi-backend/src/main/java/data/IntraDay.java
+++ quantfi-backend/src/main/java/data/IntraDay.java
@@ -1,4 +1,51 @@
 package data;
 
-public class IntraDay{
+import connection.AlphaVantageException;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class IntraDay extends StockDataResponse {
+
+    private IntraDay(final Map<String, String> metaData, final List<StockInfo> stockInfoList) {
+        super(metaData, stockInfoList);
+    }
+
+    public static IntraDay from(Interval interval, String json) {
+        IntraDay.Parser parser = new IntraDay.Parser(interval);
+        return parser.parseJson(json);
+    }
+
+    private static class Parser extends StockDataParser<IntraDay> {
+        private final Interval interval;
+
+        Parser(Interval interval) {
+            this.interval = interval;
+        }
+
+        @Override
+        String getStockDataKey() {
+            return "Stock Data (" + interval.getValue() + ")";
+        }
+
+        @Override
+        IntraDay resolve(Map<String, String> metaData, Map<String, Map<String, String>> stockData) {
+            List<StockInfo> stocks = new ArrayList<>();
+            try {
+                stockData.forEach((key, values) -> stocks.add(new StockInfo(
+                        LocalDateTime.parse(key, DATE_WITH_TIME_FORMAT),
+                        Double.parseDouble(values.get("1. open")),
+                        Double.parseDouble(values.get("2. high")),
+                        Double.parseDouble(values.get("3. low")),
+                        Double.parseDouble(values.get("4. close")),
+                        Long.parseLong(values.get("5. volume"))
+                )));
+            } catch (Exception e) {
+                throw new AlphaVantageException("API IntraDay StockInfo error");
+            }
+            return new IntraDay(metaData, stocks);
+        }
+    }
 }
