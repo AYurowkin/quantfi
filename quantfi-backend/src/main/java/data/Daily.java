d3177807c288f8bbe93ebebb0c2bb28baf95d994
Rohit Kumar <rohitkpa@gmail.com>
2018-08-29 23:45:05 -0700

    finished parser

--- quantfi-backend/src/main/java/data/Daily.java
+++ quantfi-backend/src/main/java/data/Daily.java
@@ -1,5 +1,9 @@
 package data;
 
+import connection.AlphaVantageException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,9 +13,34 @@ public class Daily extends StockDataResponse {
         super(metaData, stockInfoList);
     }
 
-//    public static Daily from(String json) {
-//        Parser parser = new Parser();
-//        return parser.parseJson(json);
-//    }
+    public static Daily from(String json) {
+        Parser parser = new Parser();
+        return parser.parseJson(json);
+    }
 
+    private static class Parser extends StockDataParser<Daily> {
+        @Override
+        String getStockDataKey() {
+            return "Stock Data (Daily)";
+        }
+
+        @Override
+        Daily resolve(Map<String, String> metaData, Map<String, Map<String, String>> stockData) {
+            List<StockInfo> stocks = new ArrayList<>();
+            try {
+                stockData.forEach((key, values) -> stocks.add(new StockInfo(
+                        LocalDate.parse(key, DATE_SIMPLE_FORMAT).atStartOfDay(),
+                        Double.parseDouble(values.get("1. open")),
+                        Double.parseDouble(values.get("2. high")),
+                        Double.parseDouble(values.get("3. low")),
+                        Double.parseDouble(values.get("4. close")),
+                        Long.parseLong(values.get("5. volume"))
+                )));
+            } catch (Exception e) {
+                throw new AlphaVantageException("API Daily StockInfo error", e);
+            }
+
+            return new Daily(metaData, stocks);
+        }
+    }
 }
