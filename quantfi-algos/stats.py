a9e353484ec09e758c0c0d0f9944ebe93dff04a9
rohitkpa <rohitkpa@gmail.com>
2019-01-23 13:06:07 -0700

    retrieved files from daily_csv_trim

--- quantfi-algos/stats.py
+++ quantfi-algos/stats.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from pathlib import Path
 import math
@@ -5,3 +6,11 @@ import matplotlib.pyplot as plt
 
 # modularize csv trim such that you can plot all moving averages for all the stocks
 path = '../quantfi-backend/data-storage/daily_csv_trim/'
+directory = os.fsencode(path)
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".csv"):
+        print(filename)
+    else:
+        continue
