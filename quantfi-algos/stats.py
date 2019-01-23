d5d56971c2c290dc30e9f47565b285c403984b3a
rohitkpa <rohitkpa@gmail.com>
2019-01-22 17:38:41 -0700

    modularize MA for all trimmed files

---
+++ quantfi-algos/stats.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from pathlib import Path
+import math
+import matplotlib.pyplot as plt
+
+# modularize csv trim such that you can plot all moving averages for all the stocks
+path = '../quantfi-backend/data-storage/daily_csv_trim/'
