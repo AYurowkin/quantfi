827dd66c6738210bf9ac1e2ab6b40808359f2d2f
rohitkpa <rohitkpa@gmail.com>
2019-03-28 21:18:48 -0700

    lstm changes

--- quantfi-algos/main.py
+++ quantfi-algos/main.py
@@ -1,5 +1,6 @@
 from util.movingAverage import MovingAverage
 from util.prep import Prep
+from util.lstm import LSTM
 
 # main class to run all tests
 
@@ -8,6 +9,6 @@ class Main:
 
     if __name__ == '__main__':
         p = Prep()
-        p.get_path('AAPL')
-        ma = MovingAverage(20, 100)
-        ma.get_one_ma_intersection('AAPL')
+        stock_df = p.get_path('AAPL')
+        lstm = LSTM()
+        lstm.run_lstm(stock_df)
--- quantfi-algos/util/lstm.py
+++ quantfi-algos/util/lstm.py
@@ -2,33 +2,35 @@ import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Dropout, LSTM, CuDNNLSTM
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train/255.0
-x_test = x_test/255.0
+class LSTM:
 
-print(type(x_train))
+    def run_lstm(self, stock_df):
+        mnist = tf.keras.datasets.mnist
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-model = Sequential()
+        x_train = x_train/255.0
+        x_test = x_test/255.0
 
-model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu', return_sequences=True))
-# model.add(CuDNNLSTM(128, input_shape=(x_train.shape[1:]), return_sequences=True))
-model.add(Dropout(0.2))
+        model = Sequential()
 
-model.add(LSTM(128, activation='relu'))
-# model.add(CuDNNLSTM(128))
-model.add(Dropout(0.2))
+        model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu', return_sequences=True))
+        # model.add(CuDNNLSTM(128, input_shape=(x_train.shape[1:]), return_sequences=True))
+        model.add(Dropout(0.2))
 
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.2))
+        model.add(LSTM(128, activation='relu'))
+        # model.add(CuDNNLSTM(128))
+        model.add(Dropout(0.2))
 
-model.add(Dense(10, activation='softmax'))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dropout(0.2))
 
-opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
+        model.add(Dense(10, activation='softmax'))
 
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
+        opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
 
-model.fit(x_train, y_train, epochs=3, validation_data=(x_test, y_test))
+        model.compile(loss='sparse_categorical_crossentropy',
+                      optimizer=opt,
+                      metrics=['accuracy'])
+
+        model.fit(x_train, y_train, epochs=3, validation_data=(x_test, y_test))
